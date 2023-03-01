7fcd51186ca03fb31bf89de5fa7d6efdbb8b4d67
RsSvc <wonderhays@gmail.com>
2023-03-01 15:04:20 +0200

    added bidask-snapshots

---
+++ src/bid_ask_snapshot_model.rs
@@ -0,0 +1,20 @@
+use serde::*;
+
+#[my_no_sql_macros::my_no_sql_entity("bidask-snapshots")]
+#[derive(Serialize, Deserialize, Debug, Clone)]
+#[serde(rename_all = "PascalCase")]
+pub struct DefaultsNoSqlEntity {
+    pub unix_timestamp_with_milis: u64,
+    pub bid: f64,
+    pub ask: f64,
+}
+
+impl DefaultsNoSqlEntity {
+    pub fn generate_partition_key() -> &'static str {
+        "instruments_snapshot"
+    }
+
+    pub fn generate_rk(instrument_id: &str) -> &'static str {
+        instrument_id
+    }
+}
