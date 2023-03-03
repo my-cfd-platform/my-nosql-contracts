0c764b998151dc24a717c1d105717907ffdb0d90
amigin <amigin@gmail.com>
2023-03-03 14:41:27 +0200

    Adde default_favorite_instruments

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,13 @@
-mod default_value;
 mod bid_ask_snapshot_model;
+mod default_favorite_instruments;
+mod default_value;
 mod trading_group;
 mod trading_instrument;
 mod trading_profile;
 
-pub use default_value::*;
 pub use bid_ask_snapshot_model::*;
+pub use default_favorite_instruments::*;
+pub use default_value::*;
 pub use trading_group::*;
 pub use trading_instrument::*;
 pub use trading_profile::*;
