0c8089122d3c642036694384f07f871405756991
RsSvc <wonderhays@gmail.com>
2023-03-03 15:24:01 +0200

    added session entity

--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,11 @@ mod bid_ask_snapshot_model;
 mod trading_group;
 mod trading_instrument;
 mod trading_profile;
+mod session_entity;
 
 pub use default_value::*;
 pub use bid_ask_snapshot_model::*;
 pub use trading_group::*;
 pub use trading_instrument::*;
 pub use trading_profile::*;
+pub use session_entity::*;
