295dda7238ef42e03a5c4482b9ea5759fe8d6c16
amigin <amigin@gmail.com>
2023-03-10 10:51:52 +0200

    Added Instrument Avatar

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,17 @@
 mod bid_ask_snapshot_model;
 mod default_favorite_instruments;
 mod default_value;
+mod instrument_avatar;
+mod session_entity;
 mod trading_group;
 mod trading_instrument;
 mod trading_profile;
-mod session_entity;
 
 pub use bid_ask_snapshot_model::*;
 pub use default_favorite_instruments::*;
 pub use default_value::*;
+pub use instrument_avatar::*;
+pub use session_entity::*;
 pub use trading_group::*;
 pub use trading_instrument::*;
 pub use trading_profile::*;
-pub use session_entity::*;
