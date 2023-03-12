96ba623edc49278f2d60cf1c68a0223245bbc4b9
amigin <amigin@gmail.com>
2023-03-12 15:43:15 +0200

    Removed session_entity

--- src/lib.rs
+++ src/lib.rs
@@ -2,16 +2,13 @@ mod bid_ask_snapshot_model;
 mod default_favorite_instruments;
 mod default_value;
 mod instrument_avatar;
-mod session_entity;
 mod trading_group;
 mod trading_instrument;
 mod trading_profile;
-
 pub use bid_ask_snapshot_model::*;
 pub use default_favorite_instruments::*;
 pub use default_value::*;
 pub use instrument_avatar::*;
-pub use session_entity::*;
 pub use trading_group::*;
 pub use trading_instrument::*;
 pub use trading_profile::*;
