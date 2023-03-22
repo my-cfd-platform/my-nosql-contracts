c182025b66576f20cb30790d80b44295588d81bf
amigin <amigin@gmail.com>
2023-03-22 09:26:56 +0200

    Made KeyValueCacheMyNoSqlEntity

---
+++ src/key_value_cache.rs
@@ -0,0 +1,20 @@
+use std::collections::HashMap;
+
+use serde::{Deserialize, Serialize};
+
+#[my_no_sql_macros::my_no_sql_entity("fav-instruments-cache")]
+#[derive(Serialize, Deserialize, Debug, Clone)]
+#[serde(rename_all = "PascalCase")]
+pub struct KeyValueCacheMyNoSqlEntity {
+    pub key_value: HashMap<String, String>,
+}
+
+impl KeyValueCacheMyNoSqlEntity {
+    pub fn generate_partition_key() -> &'static str {
+        "c"
+    }
+
+    pub fn generate_row_key(trader_id: &str) -> &str {
+        trader_id
+    }
+}
