375cb03fd2c905e9c0e4e87f17455a65a3a36b64
amigin <amigin@gmail.com>
2023-03-22 11:20:45 +0200

    Small Improvement

--- src/key_value_cache.rs
+++ src/key_value_cache.rs
@@ -1,5 +1,6 @@
 use std::collections::HashMap;
 
+use rust_extensions::{IntoStringOrStr, StrOrString};
 use serde::{Deserialize, Serialize};
 
 #[my_no_sql_macros::my_no_sql_entity("key-value-cache")]
@@ -14,7 +15,7 @@ impl KeyValueCacheMyNoSqlEntity {
         "c"
     }
 
-    pub fn generate_row_key(trader_id: &str) -> &str {
-        trader_id
+    pub fn generate_row_key<'s>(trader_id: impl IntoStringOrStr<'s>) -> StrOrString<'s> {
+        trader_id.into_string_or_str()
     }
 }
