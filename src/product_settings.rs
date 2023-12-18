e064660e2430ae68ac18c95c47add498a2ab10de
amigin <amigin@gmail.com>
2023-12-18 19:02:12 +0200

    Added product-settings

--- src/product_settings.rs
+++ src/product_settings.rs
@@ -1,3 +1,7 @@
+use serde::*;
+
+service_sdk::macros::use_my_no_sql_entity!();
+
 #[enum_of_my_no_sql_entity(table_name:"product-settings", generate_unwraps)]
 pub enum ProductSettings {
     SendGrid(SendGridSettingsModel),
@@ -5,7 +9,6 @@ pub enum ProductSettings {
 
 #[enum_model(partition_key:"emails", row_key: "send-grid")]
 #[derive(Serialize, Deserialize, Clone)]
-
 pub struct SendGridSettingsModel {
     pub send_grid_api_key: String,
     pub seq_conn_string: String,
