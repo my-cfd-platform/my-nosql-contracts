4778e47e1c7d01c31b90e8e257a5f55fb6f2c28c
amigin <amigin@gmail.com>
2023-12-18 18:14:57 +0200

    Added ProductSettings Model

---
+++ src/product_settings.rs
@@ -0,0 +1,12 @@
+#[enum_of_my_no_sql_entity(table_name:"product-settings", generate_unwraps)]
+pub enum ProductSettings {
+    SendGrid(SendGridSettingsModel),
+}
+
+#[enum_model(partition_key:"emails", row_key: "send-grid")]
+#[derive(Serialize, Deserialize, Clone)]
+
+pub struct SendGridSettingsModel {
+    pub send_grid_api_key: String,
+    pub seq_conn_string: String,
+}
--- src/swap_profile_model.rs
+++ src/swap_profile_model.rs
@@ -1,4 +1,4 @@
-use serde::{Serialize, Deserialize};
+use serde::{Deserialize, Serialize};
 service_sdk::macros::use_my_no_sql_entity!();
 #[my_no_sql_entity("swap-profile")]
 #[derive(Serialize, Deserialize, Debug, Clone)]
@@ -8,4 +8,10 @@ pub struct SwapProfileNoSqlModel {
     pub instrument_id: String,
     pub long: f64,
     pub short: f64,
+}
+
+impl SwapProfileNoSqlModel {
+    pub fn generate_partition_key(group_id: &'static str) -> &'static str {
+        group_id
+    }
 }
