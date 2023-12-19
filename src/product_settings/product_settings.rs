7aae908ab5831789f31a473f146ce6600a224185
amigin <amigin@gmail.com>
2023-12-19 17:12:28 +0200

    Added TrackBoxProductSettings

--- src/product_settings/product_settings.rs
+++ src/product_settings/product_settings.rs
@@ -9,6 +9,7 @@ service_sdk::macros::use_my_no_sql_entity!();
 #[enum_of_my_no_sql_entity(table_name:"product-settings", generate_unwraps)]
 pub enum ProductSettings {
     SendGrid(SendGridSettingsModel),
+    TrackBox(TrackBoxSettingsModel),
 }
 
 #[enum_model(partition_key:"emails", row_key: "send-grid")]
@@ -25,5 +26,17 @@ impl SendGridSettingsModel {
     pub fn get_email_id(&self, email_type: EmailType) -> Option<String> {
         let result = self.email_ids.get(email_type.as_str())?;
         Some(result.to_string())
+    }
+}
+
+#[enum_model(partition_key:"trackbox", row_key: "trackbox")]
+#[derive(Serialize, Deserialize, Clone)]
+pub struct TrackBoxSettingsModel {
+    pub api_keys: Vec<String>,
+}
+
+impl TrackBoxSettingsModel {
+    pub fn verify_track_box_key(&self, key: &String) -> bool {
+        self.api_keys.contains(key)
     }
 }
