05ec6b50a0060db119f1ccf0b52dcc696e25c007
amigin <amigin@gmail.com>
2024-04-29 11:04:12 +0200

    Added integration settings entity

---
+++ src/integration_settings_entity.rs
@@ -0,0 +1,20 @@
+use serde::{Deserialize, Serialize};
+service_sdk::macros::use_my_no_sql_entity!();
+
+#[my_no_sql_entity("integration-settings")]
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct IntegrationSettingsEntity {}
+
+impl IntegrationSettingsEntity {
+    pub fn generate_partition_key() -> &'static str {
+        "is"
+    }
+
+    pub fn generate_row_key(api_key: &str) -> &str {
+        api_key
+    }
+
+    pub fn get_api_key(&self) -> &str {
+        &self.row_key
+    }
+}
