ef74350ae665d08a568f67028b0ba4687f3842b7
amigin <amigin@gmail.com>
2024-05-07 09:19:14 +0300

    Added Documenttion to BidAsk

--- src/bid_ask_snapshot_model.rs
+++ src/bid_ask_snapshot_model.rs
@@ -17,4 +17,12 @@ impl BidAskSnapshotNoSqlEntity {
     pub fn generate_partition_key() -> &'static str {
         "instruments_snapshot"
     }
+
+    pub fn generate_row_key(instrument_id: &'static str) -> &'static str {
+        instrument_id
+    }
+
+    pub fn get_instrument_id(&self) -> &str {
+        &self.row_key
+    }
 }
