ccde7cf6bf3468cf861a209b301fc5635e1bad45
amigin <amigin@gmail.com>
2024-05-07 22:55:13 +0300

    Fixed Instrument Mapping

--- src/instrument_mapping_entity.rs
+++ src/instrument_mapping_entity.rs
@@ -4,8 +4,14 @@ use std::collections::HashMap;
 #[my_no_sql_entity("instrument-mapping")]
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct InstrumentMappingEntity {
-    #[serde(rename = "LpId")]
-    pub liquidity_provider_id: String,
     #[serde(rename = "Map")]
     pub map: HashMap<String, String>,
+}
+
+impl InstrumentMappingEntity {
+    pub const PARTITION_KEY: &'static str = "im";
+
+    pub fn get_lp_id(&self) -> &str {
+        self.row_key.as_str()
+    }
 }
