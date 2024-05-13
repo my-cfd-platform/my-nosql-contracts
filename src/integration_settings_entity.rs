a39b0e6d973ceb5557228cbba1bdceb64a13e40b
amigin <amigin@gmail.com>
2024-05-13 21:43:45 +0300

    removed deprecated

--- src/integration_settings_entity.rs
+++ src/integration_settings_entity.rs
@@ -3,29 +3,6 @@ service_sdk::macros::use_my_no_sql_entity!();
 
 #[my_no_sql_entity("integration-settings")]
 #[derive(Serialize, Deserialize, Debug, Clone)]
-#[deprecated]
-pub struct IntegrationSettingsMyNoSqlEntity {
-    #[serde(rename = "Name")]
-    name: String,
-}
-
-#[deprecated]
-impl IntegrationSettingsMyNoSqlEntity {
-    pub fn generate_partition_key() -> &'static str {
-        "is"
-    }
-
-    pub fn generate_row_key(api_key: &str) -> &str {
-        api_key
-    }
-
-    pub fn get_api_key(&self) -> &str {
-        &self.row_key
-    }
-}
-
-#[my_no_sql_entity("integration-settings")]
-#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct IntegrationTenantMyNoSqlEntity {
     pub name: String,
     pub api_keys: Vec<String>,
