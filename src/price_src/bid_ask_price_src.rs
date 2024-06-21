4073160c162c3d8ec3a8e17b272ef43594c1a946
amigin <amigin@gmail.com>
2024-06-21 16:14:58 +0300

    Fixes with BidAskPriceSrc

--- src/price_src/bid_ask_price_src.rs
+++ src/price_src/bid_ask_price_src.rs
@@ -4,14 +4,14 @@ service_sdk::macros::use_my_no_sql_entity!();
 #[my_no_sql_entity("bid-ask-src")]
 #[derive(Serialize, Deserialize, Debug, Clone)]
 
-pub struct PriceSourceIdMyNoSqlEntity {
+pub struct BidAskPriceSrc {
     pub bid: f64,
     pub ask: f64,
     pub dt: String,
     pub src_id: String,
 }
 
-impl PriceSourceIdMyNoSqlEntity {
+impl BidAskPriceSrc {
     pub fn generate_partition_key(src_id: &str) -> &str {
         &src_id
     }
@@ -21,7 +21,7 @@ impl PriceSourceIdMyNoSqlEntity {
     }
 }
 
-impl EntityWithStrKey for PriceSourceIdMyNoSqlEntity {
+impl EntityWithStrKey for BidAskPriceSrc {
     fn get_key(&self) -> &str {
         &self.row_key
     }
