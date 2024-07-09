8bd0a764219c82555a3cc296b52030046d52c678
amigin <amigin@gmail.com>
2024-07-09 21:40:48 +0300

    Fixed hideDeposit

--- src/product_settings/brand_settings.rs
+++ src/product_settings/brand_settings.rs
@@ -59,6 +59,9 @@ pub struct BrandSettings {
     #[serde(rename = "hideKycContent")]
     pub hide_kyc_content: Option<bool>,
 
+    #[serde(rename = "hideDeposit")]
+    pub hide_deposit: Option<bool>,
+
     #[serde(rename = "defaultChartTimeFrame")]
     pub default_chart_time_frame: Option<String>,
 }
