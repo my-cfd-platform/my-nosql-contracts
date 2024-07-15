51d7730508f0faa7c8ed07ebaa447286c1d5d33b
amigin <amigin@gmail.com>
2024-07-15 18:08:32 +0300

    Added hide_sign_up

--- src/product_settings/brand_settings.rs
+++ src/product_settings/brand_settings.rs
@@ -62,6 +62,9 @@ pub struct BrandSettings {
     #[serde(rename = "hideDeposit")]
     pub hide_deposit: Option<bool>,
 
+    #[serde(rename = "hideSignUp")]
+    pub hide_sign_up: Option<bool>,
+
     #[serde(rename = "defaultChartTimeFrame")]
     pub default_chart_time_frame: Option<String>,
 }
