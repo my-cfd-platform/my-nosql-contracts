76a113305b678b4b0ba1212a7115d00d34670952
RomanSavh <RomanSavh@users.noreply.github.com>
2024-10-29 15:48:54 +0200

    fixes

--- src/integration/integration_creads.rs
+++ src/integration/integration_creads.rs
@@ -1,3 +1,6 @@
+use serde::{Deserialize, Serialize};
+use service_sdk::my_no_sql_sdk::macros::my_no_sql_entity;
+use service_sdk::my_no_sql_sdk;
 #[my_no_sql_entity("integration-creads")]
 #[derive(Serialize, Deserialize, Debug, Clone)]
 #[serde(rename_all = "PascalCase")]
@@ -5,4 +8,10 @@ pub struct IntegrationCreadsNoSqlEntity {
     pub id: String,
     pub name: String,
     pub secret_key: String,
+}
+
+impl IntegrationCreadsNoSqlEntity{
+    pub fn generate_partition_key() -> &'static str {
+        "ic"
+    }
 }
